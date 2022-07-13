86907b2aba3bf0d45a4b479a9f06e969b3912ea1
Iol-lshh <markhong93@naver.com>
2022-07-13 11:17:43 +0900

    0.0.1 add member test case

---
+++ src/test/java/com/iollshh/authserver/service/MemberServiceTest.java
@@ -0,0 +1,78 @@
+package com.iollshh.authserver.service;
+
+import com.iollshh.authserver.domain.dto.MemberDto;
+import com.iollshh.authserver.domain.entity.Member;
+import com.iollshh.authserver.domain.factory.MemberFactory;
+import com.iollshh.authserver.domain.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+    @Mock
+    private MemberRepository memberRepository;
+    @Spy
+    private MemberFactory memberFactory;
+    @InjectMocks
+    private MemberService memberService;
+
+
+    @Test
+    void setNewMemberSuccess() throws Exception {
+        //#given
+        //test case
+        MemberDto memberDto = MemberDto.builder()
+                .accountId("tester2")
+                .accountType("tester")
+                .nickname("테스터")
+                .build();
+        //mock
+        Member mockMember = Member.builder()
+                .id(1l)
+                .accountId("tester2")
+                .accountType("tester")
+                .nickname("테스터")
+                .quit("0")
+                .build();
+        given(memberRepository.saveNewByDto(memberDto)).willReturn(mockMember);
+
+        //#test
+        //when
+        MemberDto resMemberDto = memberService.setNewMember(memberDto);
+        //then
+        assertNotNull(resMemberDto);
+        verify(memberRepository).saveNewByDto(memberDto);
+    }
+
+    @Test
+    void findMemberSuccess() throws Exception {
+        //#given
+        //test case
+        String accountId = "tester";
+
+        //mock
+        Member mockMember = Member.builder()
+                .id(1l)
+                .accountId("tester")
+                .accountType("tester")
+                .nickname("테스터")
+                .quit("0")
+                .build();
+        given(memberRepository.getReferenceByAccountId(accountId)).willReturn(mockMember);
+
+        //#test
+        //when
+        MemberDto resMemberDto = memberService.findMember(accountId);
+        //given
+        assertEquals(resMemberDto.getAccountId(), accountId);
+    }
+}
